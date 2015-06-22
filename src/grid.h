8dcda2f62727210c0bea7870897f705c6b28c99f
Felipe Vieira <nvieirafelipe@gmail.com>
2015-06-22 02:52:11 -0300

    Creates the grid

---
+++ src/grid.h
@@ -0,0 +1,5 @@
+void print_stage(char board[ROWS][COLUMNS]);
+void print_invalid_move();
+void print_ask_for_move(char player);
+void print_winner(char board[ROWS][COLUMNS], char player);
+void print_draw(char board[ROWS][COLUMNS]);
